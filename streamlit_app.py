22f84794f7b8c00eb7d25dd69d3cf4353dc76a22
Gabhik <41627657+Gabhik@users.noreply.github.com>
2023-09-07 13:33:23 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,9 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach, Rocket and smoothie')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.text('Hard-Boiled Free Range Egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
