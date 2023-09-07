e135d47ae36e591641b9dfb769897ab7ffa0550c
Gabhik <41627657+Gabhik@users.noreply.github.com>
2023-09-07 13:31:53 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach, Rocket and smoothie')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.text('Hard-Boiled Free Range Egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
